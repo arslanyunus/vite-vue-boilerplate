7df66981a20ad65b902ddbd527af2049bace8fae
Yunus Arslan <yunus@mobileaction.co>
2022-03-21 09:37:43 +0300

    chore: store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 import HelloWorld from '@/components/HelloWorld.vue';
+
+const store = useStore();
+const count = computed(() => store.state.count);
 </script>
 
 <template>
-    Home
-
+    <div class="prose">
+        <h2>Home</h2>
+        <p>Count: {{ count }}</p>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png" class="w-40 my-12"/>
 
     <div class="text-center">
